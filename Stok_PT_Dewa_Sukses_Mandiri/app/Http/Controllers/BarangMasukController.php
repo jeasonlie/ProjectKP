ddf277195ade1f22fd0400b4e0cddab7aff6d282
Jeason Lie <jeasonlie2207@mhs.mdp.ac.id>
2023-04-28 14:46:26 +0700

    update

--- Stok_PT_Dewa_Sukses_Mandiri/app/Http/Controllers/BarangMasukController.php
+++ Stok_PT_Dewa_Sukses_Mandiri/app/Http/Controllers/BarangMasukController.php
@@ -18,7 +18,7 @@ class BarangMasukController extends Controller
         $masuk = BarangMasuk::find($id);
 
         if($masuk){
-            $masukDetail = MasukDetail::where('id_masuk', $id)->get();
+            $masukDetail = BarangMasukDetail::where('id_masuk', $id)->get();
 
             return view("masuk.detail", compact(['masuk', 'masukDetail']));
         }else{
@@ -26,24 +26,17 @@ class BarangMasukController extends Controller
         }
     }
 
-    public function create(){
-        $this->authorize('create', Masuk::class);
-        $barang = Barang::all();
-        return view("masuk.create", compact('barang'));
-    }
-
     public function store(Request $request){
-        $this->authorize('create', Masuk::class);
         $request->validate([
             'tgl_masuk' => 'required|date',
-            'keterangan' => 'required',
+            'keterangan_masuk' => 'required',
             'id_barang' => 'required|array',
             'id_barang.*' => [
                 'required',
                 Rule::in(Barang::all()->pluck('id')->toArray())
             ],
-            'jumlah' => 'required|array',
-            'jumlah.*' => 'required|numeric|between:1,999'
+            'jumlah_masuk' => 'required|array',
+            'jumlah_masuk.*' => 'required|numeric|between:1,999'
         ]);
 
         $masuk = New Masuk();
@@ -52,35 +45,18 @@ class BarangMasukController extends Controller
         $masuk->kode_masuk = str_pad($increment, 8, "0", STR_PAD_LEFT);
 
         $masuk->tgl_masuk = $request->tgl_masuk;
-        $masuk->keterangan = $request->keterangan;
+        $masuk->keterangan_masuk = $request->keterangan_masuk;
         $masuk->save();
 
         for($i = 0; $i < count($request->id_barang); $i++){
             $masukDetail = new MasukDetail();
             $masukDetail->id_masuk = $increment;
             $masukDetail->id_barang = $request->id_barang[$i];
-            $masukDetail->jumlah = $request->jumlah[$i];
+            $masukDetail->jumlah_masuk = $request->jumlah_masuk[$i];
             $masukDetail->save();
         }
 
         $request->session()->flash("info", 'Data Barang Masuk '.str_pad($increment, 8, "0", STR_PAD_LEFT).' berhasil dibuat!');
         return redirect()->route("masuk.index");
-    }
-
-    public function validasi(Request $request, $id){
-        $masuk = Masuk::find($id);
-
-        if($masuk){
-            $this->authorize('validasi', [Masuk::class, $masuk]);
-
-            $masuk->id_validator = Auth::user()->id;
-            $masuk->tgl_validasi = now();
-            $masuk->save();
-
-            $request->session()->flash("info", "Data Barang Masuk $masuk->kode_masuk berhasil divalidasi!");
-            return redirect()->route("masuk.index");
-        }else{
-            return redirect()->route("masuk.index")->withErrors(['errors' => 'Data Barang Masuk tidak valid']);
-        }
     }
 }
