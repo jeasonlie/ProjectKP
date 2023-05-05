c06cf05fbfdac855f9e38c1b26750bd09ca9bd57
Jeason Lie <jeasonlie2207@mhs.mdp.ac.id>
2023-05-05 13:26:18 +0700

    hehehehehehehe

--- Stok_PT_Dewa_Sukses_Mandiri/app/Models/LaporanBarangDetail.php
+++ Stok_PT_Dewa_Sukses_Mandiri/app/Models/LaporanBarangDetail.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class BarangMasukDetail extends Model
+class LaporanBarangDetail extends Model
 {
     use HasFactory;
     Protected $table = 'laporan_barang_detail';
