b78eb977ae4a5aea463b2ed3f766dcd42ce7433f
Jeason Lie <jeasonlie2207@mhs.mdp.ac.id>
2023-05-10 15:09:20 +0700

    install breeze

--- Stok_PT_Dewa_Sukses_Mandiri/resources/views/dashboard.blade.php
+++ Stok_PT_Dewa_Sukses_Mandiri/resources/views/dashboard.blade.php
@@ -1,21 +1,17 @@
-@extends('layout.layout')
-@section('content')
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
 
-<div class="judul-content">
-    <div class="intro">Dashboard</div>
-    <div class="sub-intro">Selamat datang, User</div>
-</div>
-
-<div class="isi-content">
-    <div class="content-jumlah">
-        <a href="/barang">Jumlah Stock Barang</a>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    {{ __("You're logged in!") }}
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="content-masuk">
-        <a href="/laporanbarang">Jumlah Barang Masuk</a>
-    </div>
-    <div class="content-keluar">
-        <a href="/laporanbarang">Jumlah Barang keluar</a>
-    </div>
-</div>
-
-@endsection
+</x-app-layout>
