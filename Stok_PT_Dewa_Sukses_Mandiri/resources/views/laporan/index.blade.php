33e54cc59c4f35ea61986cdd0ce018204e688c5c
Jeason Lie <jeasonlie2207@mhs.mdp.ac.id>
2023-05-12 14:58:25 +0700

    yeheheha

--- Stok_PT_Dewa_Sukses_Mandiri/resources/views/laporan/index.blade.php
+++ Stok_PT_Dewa_Sukses_Mandiri/resources/views/laporan/index.blade.php
@@ -1,43 +1,76 @@
 @extends('layout.layout')
 @section('content')
-    <div class="title">
+<div class="title">
         Laporan Barang
     </div>
     <br>
     <div class="input">
-        <form class="store">
-            <label for="">
-                Nama Barang
-            </label>
-            <br>
-            <input type="text" name="nama" id="">
-            <br>
-            <label for="">
-                Stock
-            </label>
-            <br>
-            <input type="number" name="stok" id="">
-            <br><br>
-            <button onClick = 'storeData()'>
-                Tambah 
+        <form action="">
+            <button>
+                Tambah Barang
             </button>
-            <input type="hidden" id="csrfToken" value="{{ csrf_token()}}">
+            <br><br>
+            <table id="form">
+                <tr>
+                    <th>
+                        ID Barang
+                    </th>
+                    
+                    <th>
+                        Jumlah Masuk/Keluar
+                    </th>
+                </tr>
+                <tr>
+                    <td>
+                    <input type="number" name="id_barang" id="">
+                    </td>
+                    <td>
+                    <input type="number" name="jumlah" id="">
+                    </td>
+                </tr>
+            </table>
+            <br>
+            <label for="">
+                Keterangan
+            </label>
+            <br>
+            <input type="text" name="keterangan">
+            <br><br>
+            <button>
+                Tambah
+            </button>
         </form>
     </div>
     <br><br>
     <div class="table">
-        <table>
+        <table id="isi">
             <thead>
                 <tr>
                     <th>No</th>
                     <th>Nama Barang</th>
-                    <th>Jumlah Masuk/Keluar Barang</th>
+                    <th>Jumlah Masuk/Keluar</th>
                     <th>Keterangan</th>
                     <th>Tanggal</th>
                 </tr>
             </thead>
             <tbody>
-                
+                <tr>
+                    <td>
+
+                    </td>
+                    <td>
+
+                    </td>
+                    <td>
+
+                    </td>
+                    <td>
+
+                    </td>
+                    <td>
+
+                    </td>
+                </tr>
             </tbody>
         </table>
     </div>
@@ -45,9 +78,7 @@
 @section('script')
 
 <script>
-    const formStore = document.querySelector('form.store');
-    const innerHTML = formStore.innerHTML;
-    const url = window.location.origin+"/api/barang";
+    const url = window.location.origin+"/api/laporanbarang";
     getData();
     async function getData(){
         try{
@@ -56,58 +87,51 @@
             const table = document.querySelector('table tbody');
             let rows = '';
             data.data.forEach((element,index) => {
-                const newRow = 
-                `
+                const form = document.createElement('form');
+                form.innerHTML = `
                     <tr>
-                        <td>
-                            ${++index}
-                        </td>
-                        <td>
-                            ${element.nama_barang}
-                        </td>
-                        <td>
-                            ${element.stok}
-                        </td>
-                        <td>
-                            <button class="edit-${element.id}"onClick = 'edit(${JSON.stringify(element)});'>
-                                Edit
-                            </button>
-                        </td>
-                        <td>
-                            <button onClick = 'deleteData(${element.id})'>
-                                Delete
-                            </button>
-                        </td>
+                            <td>
+                                ${++index}
+                            </td>
+                            <td>
+                                <input class="input-${element.id}" type="text" name="nama" id="" value="${element.nama_barang}" readonly>
+                            </td>
+                            <td>
+                                <input class="input-${element.id}" type="text" name="stok" id="" value="${element.stok}" readonly>
+                            </td>
+                            <td>
+                                <button class="edit-${element.id}"onClick = 'toggle(${element.id});'>
+                                    Edit
+                                </button>
+                                <button class="simpan-${element.id}" onClick = 'updateData(${element.id})' style = 'display:none;'>
+                                    Simpan
+                                </button>
+                            </td>
+                            <td>
+                                <button onClick = 'deleteData(${element.id})'>
+                                    Delete
+                                </button>
+                            </td>
                     </tr>
                 `
-                rows += newRow;
+                form.id = element.id;
+                rows += form;
             });
-            table.innerHTML = rows;
+            table.appendChild(rows);
         }
         catch(err){
             console.error(err);
         }
     }
-    function edit(obj){
-        formStore.innerHTML = 
-        `
-        <label for="">
-                Nama Barang
-            </label>
-            <br>
-            <input type="text" name="nama" id="" value=${obj.nama_barang}>
-            <br>
-            <label for="">
-                Stock
-            </label>
-            <br>
-            <input type="number" name="stok" id="" value=${obj.stok}>
-            <br><br>
-            <button onClick = 'updateData(${obj.id})'>
-                Simpan
-            </button>
-            <input type="hidden" id="csrfToken" value="{{ csrf_token()}}">
-        `
+    function toggle(id){
+        const inputs = document.querySelectorAll('.input-'+id);
+        inputs.forEach(input => {
+            input.readOnly = !input.readOnly;
+        });
+        const edits = document.querySelector('.edit-'+id);
+        edits.style.display = 'none';
+        const simpans = document.querySelector('.simpan-'+id);
+        simpans.style.display = 'block';
     }
     async function storeData(){
         event.preventDefault();
@@ -134,31 +158,31 @@
         event.preventDefault();
         try {
             const crsfToken = document.querySelector('#csrfToken').value;
-            const form = document.querySelector('form.store');
+            const form = document.querySelector('#form-'+id);
             const formData = new FormData(form);
+            console.log(form.innerHTML);
+            console.log(url+'/'+id);
             const response = await fetch(url+'/'+id,{
                 headers: {
                     "Content-Type": "application/json",
                     "X-CSRF-Token": csrfToken
                 },
-                method: "post",
+                method: "patch",
                 credentials: "same-origin",
                 body: JSON.stringify(Object.fromEntries(formData))
             });
             await getData();
-            tambah();
         } 
         catch (err) {
             console.error(err);
         }
     }
-    function tambah() {
-        formStore.innerHTML = innerHTML;
-    }
     async function deleteData(id){
         event.preventDefault();
         try {
             const crsfToken = document.querySelector('#csrfToken').value;
+            const form = document.querySelector('#form-'+id);
+            const formData = new FormData(form);
             const response = await fetch(url+'/'+id,{
                 headers: {
                     "Content-Type": "application/json",
@@ -166,6 +190,7 @@
                 },
                 method: "delete",
                 credentials: "same-origin",
+                body: JSON.stringify(Object.fromEntries(formData))
             });
             await getData();
         } 
