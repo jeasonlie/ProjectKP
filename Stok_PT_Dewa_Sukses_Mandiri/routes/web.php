5c74a79295012c736a7812307981a8fe5faf6de1
Jeason Lie <jeasonlie2207@mhs.mdp.ac.id>
2023-05-30 20:22:52 +0700

    yessirrrrrr767676

--- Stok_PT_Dewa_Sukses_Mandiri/routes/web.php
+++ Stok_PT_Dewa_Sukses_Mandiri/routes/web.php
@@ -18,14 +18,7 @@ use App\Http\Controllers\UserController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::get('/dashboard', [DashboardController::class,'index'])->name('dashboard');
     Route::get('/barang', [BarangController::class,'index']);
     Route::get('/transaksibarang', [LaporanBarangController::class,'index']);
